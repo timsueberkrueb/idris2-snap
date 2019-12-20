fe2697fe8a65823203ab57b04654ab3bbbbc73f0
Tim Süberkrüb <dev@timsueberkrueb.io>
2019-12-20 16:59:36 +0100

    Fix build

--- src/chez.sh
+++ src/chez.sh
@@ -3,6 +3,6 @@
 SNAP="${SNAP:-/snap/idris2/current}"
 
 # Point bundled scheme to bundled *.boot files
-$SNAP/usr/bin/scheme \
+$SNAP/usr/bin/chezscheme9.5 \
     -b $SNAP/usr/lib/csv*/ta6le/petite.boot \
     -b $SNAP/usr/lib/csv*/ta6le/scheme.boot $@
